2702b2c5c17b47209f3681ee9f10e743e895127a
Stev Witzel <switzel@pivotal.io>
2015-11-27 14:45:07 +0000

    Switch to latest version of cf-test-helpers
    [#108293034]

--- service/service_suite_test.go
+++ service/service_suite_test.go
@@ -2,43 +2,63 @@ package service_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"testing"
 
-	"github.com/cloudfoundry-incubator/cf-test-helpers/services/context_setup"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/services"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-type redisTestConfig struct {
-	context_setup.IntegrationConfig
+type testConfig struct {
+	services.Config
 
 	ServiceName string   `json:"service_name"`
 	PlanNames   []string `json:"plan_names"`
 }
 
-func loadConfig() (testConfig redisTestConfig) {
-	path := os.Getenv("CONFIG_PATH")
+func loadConfig(path string) (cfg testConfig) {
 	configFile, err := os.Open(path)
 	if err != nil {
-		panic(err)
+		fatal(err)
 	}
 
 	decoder := json.NewDecoder(configFile)
-	err = decoder.Decode(&testConfig)
-	if err != nil {
-		panic(err)
+	if err = decoder.Decode(&cfg); err != nil {
+		fatal(err)
 	}
 
-	return testConfig
+	return
 }
 
-var config = loadConfig()
+var (
+	config = loadConfig(os.Getenv("CONFIG_PATH"))
+	ctx    services.Context
+)
+
+func fatal(err error) {
+	fmt.Printf("ERROR: %s\n", err.Error())
+	os.Exit(1)
+}
 
 func TestService(t *testing.T) {
-	context_setup.TimeoutScale = 3
-	context_setup.SetupEnvironment(context_setup.NewContext(config.IntegrationConfig, "rabbitmq-smoke-tests"))
+	if err := services.ValidateConfig(&config.Config); err != nil {
+		fatal(err)
+	}
+
+	ctx = services.NewContext(config.Config, "rabbitmq-smoke-tests")
+
 	RegisterFailHandler(Fail)
+
 	RunSpecs(t, "RabbitMQ Smoke Tests")
 }
+
+var _ = BeforeEach(func() {
+	ctx.Setup()
+})
+
+var _ = AfterEach(func() {
+	ctx.Teardown()
+})
--- service/service_test.go
+++ service/service_test.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/runner"
-	"github.com/cloudfoundry-incubator/cf-test-helpers/services/context_setup"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -33,11 +32,11 @@ var _ = Describe("RabbitMQ Service", func() {
 
 	BeforeEach(func() {
 		appName = randomName()
-		Eventually(cf.Cf("push", appName, "-m", "256M", "-p", appPath, "-s", "cflinuxfs2", "-no-start"), context_setup.ScaledTimeout(timeout)).Should(Exit(0))
+		Eventually(cf.Cf("push", appName, "-m", "256M", "-p", appPath, "-s", "cflinuxfs2", "-no-start"), config.ScaledTimeout(timeout)).Should(Exit(0))
 	})
 
 	AfterEach(func() {
-		Eventually(cf.Cf("delete", appName, "-f"), context_setup.ScaledTimeout(timeout)).Should(Exit(0))
+		Eventually(cf.Cf("delete", appName, "-f"), config.ScaledTimeout(timeout)).Should(Exit(0))
 	})
 
 	AssertLifeCycleBehavior := func(planName string) {
@@ -45,31 +44,31 @@ var _ = Describe("RabbitMQ Service", func() {
 			serviceInstanceName := randomName()
 
 			createServiceSession := cf.Cf("create-service", config.ServiceName, planName, serviceInstanceName)
-			createServiceSession.Wait(context_setup.ScaledTimeout(timeout))
+			createServiceSession.Wait(config.ScaledTimeout(timeout))
 
 			createServiceStdout := createServiceSession.Out
 
 			select {
 			case <-createServiceStdout.Detect("FAILED"):
-				Eventually(createServiceSession, context_setup.ScaledTimeout(timeout)).Should(Say("10001"))
-				Eventually(createServiceSession, context_setup.ScaledTimeout(timeout)).Should(Exit(1))
+				Eventually(createServiceSession, config.ScaledTimeout(timeout)).Should(Say("10001"))
+				Eventually(createServiceSession, config.ScaledTimeout(timeout)).Should(Exit(1))
 				fmt.Println("No Plan Instances available for testing plan:", planName)
 			case <-createServiceStdout.Detect("OK"):
-				Eventually(createServiceSession, context_setup.ScaledTimeout(timeout)).Should(Exit(0))
-				Eventually(cf.Cf("bind-service", appName, serviceInstanceName), context_setup.ScaledTimeout(timeout)).Should(Exit(0))
-				Eventually(cf.Cf("start", appName), context_setup.ScaledTimeout(5*time.Minute)).Should(Exit(0))
+				Eventually(createServiceSession, config.ScaledTimeout(timeout)).Should(Exit(0))
+				Eventually(cf.Cf("bind-service", appName, serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+				Eventually(cf.Cf("start", appName), config.ScaledTimeout(5*time.Minute)).Should(Exit(0))
 
 				uri := appUri(appName) + "/store"
 				fmt.Println("Posting to url: ", uri)
-				Eventually(runner.Curl("-d", "myvalue", "-X", "POST", uri, "-k", "-f"), context_setup.ScaledTimeout(timeout), retryInterval).Should(Exit(0))
+				Eventually(runner.Curl("-d", "myvalue", "-X", "POST", uri, "-k", "-f"), config.ScaledTimeout(timeout), retryInterval).Should(Exit(0))
 				fmt.Println("\n")
 
 				fmt.Println("Getting from url: ", uri)
-				Eventually(runner.Curl(uri, "-k"), context_setup.ScaledTimeout(timeout), retryInterval).Should(Say("myvalue"))
+				Eventually(runner.Curl(uri, "-k"), config.ScaledTimeout(timeout), retryInterval).Should(Say("myvalue"))
 				fmt.Println("\n")
 
-				Eventually(cf.Cf("unbind-service", appName, serviceInstanceName), context_setup.ScaledTimeout(timeout)).Should(Exit(0))
-				Eventually(cf.Cf("delete-service", "-f", serviceInstanceName), context_setup.ScaledTimeout(timeout)).Should(Exit(0))
+				Eventually(cf.Cf("unbind-service", appName, serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+				Eventually(cf.Cf("delete-service", "-f", serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
 			}
 			createServiceStdout.CancelDetects()
 
