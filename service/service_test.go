6faa7dccd117cf9e4dfbeed805c63fc8289f5303
Chavdar Chernashki <cchernashki@pivotal.io>
2016-05-05 15:23:21 +0100

    Update Godeps version + change link from code.google.com to github
    [#117591737]
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- service/service_test.go
+++ service/service_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"code.google.com/p/go-uuid/uuid"
+	"github.com/pborman/uuid"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/runner"
