515e19e8a3bc444987dc04fd894701ff54d2e11d
Denise Yu <dyu@pivotal.io>
2017-08-30 15:56:24 +0100

    Make the spring app cf pushable
    - create a rabbit security group
    - add a cf manifest
    - fix missing spring configuration beans
    
    [#144012923]
    
    Signed-off-by: Diego Lemos <dlresende@pivotal.io>

---
+++ assets/spring-example-app/README.md
@@ -0,0 +1,9 @@
+```
+gradle build
+cf create-service p-rabbitmq standard rabbitmq
+cf create-security-group rabbitmq asg-rabbitmq.yml
+cf bind-security-group rabbitmq system system
+cf push
+curl -X POST http://pivotal-rabbitmq.bosh-lite.com/queues/<queueName> -d "the
+message contents" -H "content-type: text/plain"
+```
--- assets/spring-example-app/src/main/java/io/pivotal/pcf/rabbitmq/springexampleapp/SpringExampleAppApplication.java
+++ assets/spring-example-app/src/main/java/io/pivotal/pcf/rabbitmq/springexampleapp/SpringExampleAppApplication.java
@@ -1,10 +1,30 @@
 package io.pivotal.pcf.rabbitmq.springexampleapp;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringExampleAppApplication {
+
+	@Bean
+	Queue queue() {
+		return new Queue("test", false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("spring-boot-exchange");
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("test");
+	}
 
 	public static void main(String[] args) {
  		SpringApplication.run(SpringExampleAppApplication.class, args);
